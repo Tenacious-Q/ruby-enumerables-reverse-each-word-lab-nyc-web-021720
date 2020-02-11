fd00aa144de961bb3cc916d215c5e91f5cec400c
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-11 19:40:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 
-def reverse_each_word(sentence)
+# Note to self: don't forget about the #split and #join methods
 
+def reverse_each_word(sentence)
   array_of_words = sentence.split(" ")
   new_array = []
-  array_of_words.each do |word|
+  array_of_words.collect do |word|
     new_array << word.reverse
   end
   new_array.join(" ")
